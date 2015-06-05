182095ad62192e376d4261f3c54e92ece161cb00
unknown <b.santana@ERP-BRUNO.SEDASAO>
2015-06-05 17:56:11 -0300

    commit inicial com implementacao ate a metade do capitulo 5 do livro

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express')
+, load = require('express-load')
+, bodyParser = require('body-parser')
+, cookieParser = require('cookie-parser')
+, expressSession = require('express-session')
+, methodOverride = require('method-override')
+, app = express();
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(cookieParser('ntalk'));
+app.use(expressSession());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
+app.use(express.static(__dirname + '/public'));
+
+load('models')
+.then('controllers')
+.then('routes')
+.into(app);
+
+app.listen(3000, function(){
+	console.log("Ntalk no ar.");
+});
